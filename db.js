b097c086eb51187b77ef0a134dcc9686ec33a3ba
User <user@Users-Mac.local>
2015-07-03 20:59:22 -0700

    mongodb update

--- db.js
+++ db.js
@@ -1,6 +1,12 @@
 var mongoose = require('mongoose');
+var url = process.env.MONGOLAB_URI || 'mongodb://localhost/bubbles'
+
+mongoose.connect(ur);
+
+/*
 mongoose.connect('mongodb://localhost/bubbles', function(){
 	console.log('mongodb connected')
 });
+*/
 
 module.exports = mongoose;
