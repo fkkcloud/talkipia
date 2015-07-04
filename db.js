06e57ce962ea89000b4a41fbe4e85da08113eb99
User <user@Users-Mac.local>
2015-07-03 21:29:14 -0700

    map lib crash test

--- db.js
+++ db.js
@@ -1,7 +1,9 @@
 var mongoose = require('mongoose');
 var url = process.env.MONGOLAB_URI || 'mongodb://localhost/bubbles'
 
-mongoose.connect(url);
+mongoose.connect(url, function(){
+	console.log('mongodb connected');
+});
 
 /*
 mongoose.connect('mongodb://localhost/bubbles', function(){
