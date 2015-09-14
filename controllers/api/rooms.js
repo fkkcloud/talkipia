3bb75b2de160d93fab0a55391c34fb4247790abb
Jae Hyun Yoo <fkkcloud@gmail.com>
2015-09-13 18:51:48 -0700

    create place in model of room2

--- controllers/api/rooms.js
+++ controllers/api/rooms.js
@@ -160,7 +160,7 @@ router.post('/update_view', cors(), function(req, res, next){
     	Room.find(query, function(err, room){
     		if (err) return res.send(500, { error: err });
 
-    		websockets.broadcast('update_update_view', room);
+    		websockets.broadcast('update_room_view', room);
     		res.status(200).json(room);
     	});
 
