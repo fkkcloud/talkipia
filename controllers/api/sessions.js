91e6f36bdb350b8b4c3d731f4425e257670b2c9a
Jae Hyun Yoo <fkkcloud@gmail.com>
2015-09-25 01:33:52 -0700

    duplicated session block id

--- controllers/api/sessions.js
+++ controllers/api/sessions.js
@@ -197,7 +197,7 @@ router.post('/update_blocklist', cors(), function(req, res, next){
 // for updating current location constantly
 router.post('/clear_blocklist', cors(), function(req, res, next){
 	var query       = {'guid'     : req.body.guid};
-	var emptylist  	= {'blocklist': "[\"1234\"]"b};
+	var emptylist  	= {'blocklist': "[\"1234\"]"};
 
 	Session.findOneAndUpdate(query, emptylist, function(err, session){
     	if (err) res.send(500, { error: err });
