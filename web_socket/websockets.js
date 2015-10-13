9442c3ecbf28cf18bd2af592af8531f332d58a46
Jae Hyun Yoo <fkkcloud@gmail.com>
2015-10-12 23:09:44 -0700

    server socket guid receiving packet

--- web_socket/websockets.js
+++ web_socket/websockets.js
@@ -4,6 +4,20 @@ var ws = require('ws');
 var clients = [];
 var clients_guid = [];
 
+var clients_table = {};
+
+exports.getClient = function(guid){
+	return clients_table[guid];
+}
+
+exports.broadcastTo = function(guid_list, data){
+	var json = JSON.stringify({type: type, data: data});
+
+	guid_list.forEach(function(guid){
+		clients_table[guid].send(json)
+	});
+}
+
 exports.getClients = function(){
 	return clients;
 };
@@ -17,19 +31,29 @@ exports.connect = function(server){
 		console.log('new client connected.');
 
 		clients.push(ws);
-
-		// save ws with guid as key to clinets array
-		ws.on('message', function(guid){
-			clients_guid[guid] = ws; 
-		})
 		
 		exports.broadcast('server said - new client connected.'); // 클라이언트가 서버의 웹소켓에 접속할때 broadcast
+
+		/* this is for saving ws to corresponding guid */
+		ws.on('message', function incoming(message){
+			console.log('received message: %s', message);
+
+			var received_package = JSON.parse(message);
+			var guid = received_package.guid;
+			clients_table[guid] = ws;
+		})
 
 		// 클라이언트가 연결을 끊으면 Lo-Dash를 사용해 목록에서 클라이언트를 제거한다.
 		// 연결이 끊어진 클라이언트로 메세지를 송신하려 시도하면 고약한 에러가 발생하므로,
 		// 꼭 끊어줘야 된다는 것을 명심하자!
 		ws.on('close', function(){ 
 			console.log('user socket closed.');
+
+			for (var key in clients_table) {
+			  if (clients_table.hasOwnProperty(key) && clients_table[key] == ws) {
+			    delete clients_table[key];
+			  }
+			}
 
 			_.remove(clients, ws);
 		});
@@ -43,11 +67,4 @@ exports.broadcast = function(type, data){
 	clients.forEach(function(client){
 		client.send(json)
 	});
-
-	/*
-	// 각각의 클라이언트에게 json을 보낸다.
-	clients.forEach(function(client){
-		client.send(json);
-	});
-	*/
 };
